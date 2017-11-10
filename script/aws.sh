6cb3743b49b63b6f37839603c919e7dd9caabc1f
haoyifan <haoyifan@umich.edu>
2017-11-09 19:10:50 -0600

    Add more ECS machines

--- script/aws.sh
+++ script/aws.sh
@@ -6,6 +6,14 @@ elif [ $1 == "aws2" ]; then
     ssh -i demo.pem ubuntu@54.80.33.179
 elif [ $1 == "aws3" ]; then
     ssh -i demo.pem ubuntu@184.73.149.187
+elif [ $1 == "aws4" ]; then
+    ssh -i demo.pem ubuntu@34.205.129.96
+elif [ $1 == "aws5" ]; then
+    ssh -i demo.pem ubuntu@54.166.208.38
+elif [ $1 == "aws6" ]; then
+    ssh -i demo.pem ubuntu@54.173.198.30
+elif [ $1 == "aws7" ]; then
+    ssh -i demo.pem ubuntu@174.129.141.218
 else
     echo "invalid aws instance"
     exit 1
